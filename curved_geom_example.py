b9dc8f27ad462127626887c9278d5d0b84c0a98d
Anna Yesypenko <anna@oden.utexas.edu>
2025-03-17 19:10:41 -0500

    curved geom example in progress

---
+++ curved_geom_example.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+from hps.pdo               import PDO2d,PDO3d,const,get_known_greens
+from hps.geom              import BoxGeometry, ParametrizedGeometry2D
+
+from hps.hps_multidomain   import HPSMultidomain
+from hps.fd_discretization import FDDiscretization
+from time import time
+from matplotlib import pyplot as plt
+
+
+a = 1/8; p = 10; kh = 8
+
+#############################################################
+
+### Function z maps from reference domain (rectangle) to curved domain
+mag = 0.25
+psi  = lambda z: 1 - mag * np.sin(6*z)
+z1   = lambda xx: xx[:,0]
+z2   = lambda xx: np.divide(xx[:,1],psi(xx[:,0]))
+
+### Function y is the inverse map
+### Use Wolfram Alpha to compute 2 partial derivatives wrt y,
+### (some are zero and omitted).
+y1   = lambda xx: xx[:,0]
+y2   = lambda xx: np.multiply(xx[:,1],psi(xx[:,0]))
+
+dpsi   = lambda z:   - mag*6 * np.cos(6*z)
+ddpsi  = lambda z:     mag*36  * np.sin(6*z)
+
+y1_d1  = lambda xx: np.ones(xx[:,0].shape)
+y2_d1  = lambda xx: np.multiply(xx[:,1], dpsi(xx[:,0]))
+y2_d2  = lambda xx: psi(xx[:,0])
+
+y2_d1d1  = lambda xx: np.multiply(xx[:,1], ddpsi(xx[:,0]))
+
+#############################################################
+## We would like to solve the constant-coefficient Helmholtz equation
+## on the parameterized geometry.
+
+box_geom   = np.array([[0,0],[1.0,0.5]])
+param_geom = ParametrizedGeometry2D(box_geom, z1, z2, y1, y2,\
+    y1_d1=y1_d1, y2_d1=y2_d1, y2_d2=y2_d2,y2_d1d1=y2_d1d1)
+
+def bfield_constant(xx,kh):
+    return -(kh**2) * np.ones(xx.shape[0])
+
+pdo_mod = param_geom.transform_helmholtz_pdo(bfield_constant, kh)
+
+solver  = HPSMultidomain(pdo_mod,param_geom,a,p)
--- hps/geom.py
+++ hps/geom.py
@@ -1,5 +1,9 @@
 from   abc   import ABCMeta, abstractmethod, abstractproperty
 import numpy as np
+from   hps.pdo import PDO2d
+
+
+############################################################################################
 
 class AbstractGeometry(metaclass=ABCMeta):
 
@@ -14,3 +18,128 @@ class BoxGeometry(AbstractGeometry):
     @property
     def bounds(self):
         return self.box_geom
+
+
+#############################################################################################
+
+
+class ParametrizedGeometry2D(AbstractGeometry):
+
+    ## zz_func maps values on the reference domain to the curved domain
+    ## yy_func maps values on the curved domain    to the reference.
+    ## The reference domain is a rectangle defined by box_geom.
+    def __init__(self,box_geom,z1,z2,y1,y2,\
+        y1_d1=None, y1_d2=None, y2_d1=None, y2_d2=None,\
+        y1_d1d1=None, y1_d2d2=None, y2_d1d1=None, y2_d2d2=None):
+
+        self.box_geom = box_geom
+        self.zz       = (z1,z2)
+        self.yy       = (y1,y2)
+        self.yy_deriv = (y1_d1, y1_d2, y2_d1, y2_d2,\
+            y1_d1d1, y1_d2d2, y2_d1d1, y2_d2d2)
+
+    @property
+    def bounds(self):
+        return self.box_geom
+
+    @property
+    def parameter_map(self):
+        def param_map(xx):
+            (z1,z2) = self.zz
+            ZZ = xx.copy()
+            ZZ[:,0] = z1(xx)
+            ZZ[:,1] = z2(xx)
+            return ZZ
+        return param_map
+
+    @property
+    def inv_parameter_map(self):
+        def inv_param_map(xx):
+            (y1,y2) = self.yy
+            YY = xx.copy()
+            YY[:,0] = y1(xx)
+            YY[:,1] = y2(xx)
+            return YY
+        return inv_param_map
+
+    def transform_helmholtz_pdo(self,bfield,kh):
+
+        (y1_d1, y1_d2, y2_d1, y2_d2,\
+        y1_d1d1, y1_d2d2, y2_d1d1, y2_d2d2) = self.yy_deriv
+
+        if ((y1_d1 is None) and (y1_d2 is None)):
+            c11 = None
+        else:
+            def c11(xx):
+                yy = self.parameter_map(xx)
+
+                result = 0
+                if (y1_d1 is not None):
+                    result += y1_d1(yy)**2
+                if (y1_d2 is not None):
+                    result += y1_d2(yy)**2
+                return result
+
+        if ((y2_d1 is None) and (y2_d2 is None)):
+            c22 = None
+        else:
+
+            def c22(xx):
+                yy = self.parameter_map(xx)
+
+                result = 0
+                if (y2_d1 is not None):
+                    result += y2_d1(yy)**2
+                if (y2_d2 is not None):
+                    result += y2_d2(yy)**2
+                return result
+
+        if ((y1_d1d1 is None) and (y1_d2d2 is None)):
+            c1 = None
+        else:
+
+            def c1(xx):
+                yy = self.parameter_map(xx)
+
+                result = 0
+                if (y1_d1d1 is not None):
+                    result -= y1_d1d1(yy)
+                if (y1_d2d2 is not None):
+                    result -= y1_d2d2(yy)
+                return result
+
+        if ((y2_d1d1 is None) and (y2_d2d2 is None)):
+            c2 = None
+        else:
+
+            def c2(xx):
+                yy = self.parameter_map(xx)
+
+                result = 0
+                if (y2_d1d1 is not None):
+                    result -= y2_d1d1(yy)
+                if (y2_d2d2 is not None):
+                    result -= y2_d2d2(yy)
+                return result
+
+        bool_expr1 = (y1_d1 is not None) and (y2_d1 is not None)
+        bool_expr2 = (y1_d2 is not None) and (y2_d2 is not None) 
+        if ((not bool_expr1) and (not bool_expr2)):
+            c12 = None
+        else:
+
+            def c12(xx):
+                yy = self.parameter_map(xx)
+
+                result = 0
+                if (bool_expr1):
+                    result += np.multiply(y1_d1(yy),y2_d1(yy))
+                if (bool_expr2):
+                    result += np.multiply(y1_d2(yy),y2_d2(yy))
+                return result
+    
+        def c(xx):
+            return bfield(self.parameter_map(xx),kh)
+    
+        return PDO2d(c11=c11,c22=c22,c1=c1,c2=c2,c12=c12,c=c)
+    
